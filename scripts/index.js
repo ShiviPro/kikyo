d4c72f4269267cd7ccd294689b1e1bfcbe7efbbc
Shivam Tewari <43674175+ShiviPro@users.noreply.github.com>
2026-01-09 12:06:11 +0000

    feat: :sparkles: render forms dynamically
    The sample form data added in the previous commit is now used to render concise form description on the homepage

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,7 @@ const formData = [
   {
     id: 1,
     title: "My Most Recent Form",
+    thumbnailImgSrc: "/images/filled-form.png",
     createdBy: {
       mailId: "john.doe@gmail.com",
     },
@@ -15,14 +16,13 @@ const formData = [
         lastOpenedAt: new Date(2026, 0, 6, 22, 12, 10),
       },
     ],
-    viewers: [
-      "anybody"
-    ],
-    content: `Form Markdown`
+    viewers: ["anybody"],
+    content: `Form Markdown`,
   },
   {
     id: 2,
     title: "An Earlier Form",
+    thumbnailImgSrc: "/images/filled-form.png",
     createdBy: {
       mailId: "john.doe@gmail.com",
     },
@@ -39,22 +39,60 @@ const formData = [
         mailId: "jane.doe@gmail.com",
         lastModifiedAt: new Date(2026, 0, 3, 9, 25, 45),
         lastOpenedAt: new Date(2026, 0, 8, 22, 10, 10),
-      }
+      },
     ],
     viewers: [
       {
-        mailId: "trinity@gmail.com", 
+        mailId: "trinity@gmail.com",
         lastOpenedAt: new Date(2026, 0, 5, 17, 46, 12),
       },
       {
         mailId: "neo@gmail.com",
         lastOpenedAt: new Date(2026, 0, 5, 19, 25, 42),
-      }, 
+      },
       {
         mailId: "morpheus@gmail.com",
-        lastOpenedAt: new Date(2026, 0, 5, 22, 14, 57), 
-      }
+        lastOpenedAt: new Date(2026, 0, 5, 22, 14, 57),
+      },
     ],
-    content: `All form data in Markdown format`
+    content: `All form data in Markdown format`,
   },
 ];
+
+const allFormsContainer = document.getElementById("allFormsContainer");
+const months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "June",
+  "July",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
+formData.map((currFormData) => {
+  const lastOpenedAt = currFormData.lastOpenedAt;
+  const currForm = document.createElement("div");
+  currForm.classList.add("col-3");
+  currForm.innerHTML = `
+    <section class="card">
+      <img
+        src=${currFormData.thumbnailImgSrc}
+        alt="Form Thumbnail"
+        class="card-img-top p-3 pb-0 w-50 mx-auto"
+      />
+      <div class="card-body">
+        <h6 class="card-title">${currFormData.title}</h6>
+        <small class="card-text">${lastOpenedAt.getDate()} ${
+    months[lastOpenedAt.getMonth()]
+  } ${lastOpenedAt.getFullYear()}</small>
+      </div>
+    </section>
+  `;
+  allFormsContainer.appendChild(currForm);
+});
