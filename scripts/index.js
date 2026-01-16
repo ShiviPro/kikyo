59c7d7b01d452235117f53555c520c5bc2fccd62
Shivam Tewari <43674175+ShiviPro@users.noreply.github.com>
2026-01-16 14:24:57 +0000

    feat: :sparkles: add list view and the ability to toggle views
    Note: This enables the view toggle button. So now it's not just a placeholder

--- scripts/index.js
+++ scripts/index.js
@@ -59,7 +59,6 @@ const formData = [
   },
 ];
 
-const allFormsContainer = document.getElementById("allFormsContainer");
 const months = [
   "Jan",
   "Feb",
@@ -74,8 +73,16 @@ const months = [
   "Nov",
   "Dec",
 ];
+let currView = "grid";
+const allFormsContainer = document.getElementById("allFormsContainer");
+const viewToggle = document.getElementById("viewToggle");
 
-const displayAllForms = (formData) => {
+const displayAllFormsInGrid = (formData) => {
+  viewToggle.innerHTML = `<i class="fa-solid fa-list"></i>`;
+  allFormsContainer.innerHTML = "";
+  const allForms = document.createElement("div");
+  allForms.classList.add("row");
+
   formData.map((currFormData) => {
     const lastOpenedAt = currFormData.lastOpenedAt;
     const currForm = document.createElement("div");
@@ -95,8 +102,72 @@ const displayAllForms = (formData) => {
       </div>
     </section>
   `;
-    allFormsContainer.appendChild(currForm);
+    allForms.appendChild(currForm);
   });
+  allFormsContainer.appendChild(allForms);
 };
 
-displayAllForms(formData);
+const displayAllFormsInList = (formData) => {
+  viewToggle.innerHTML = `<i class="fa-solid fa-table-cells"></i>`;
+  allFormsContainer.innerHTML = "";
+  const allForms = document.createElement("table");
+  allForms.classList.add("table", "table-responsive-md", "table-hover");
+  allForms.innerHTML = `
+    <thead>
+      <tr>
+        <th></th>
+        <th>Owned By</th>
+        <th>Last Opened At</th>
+        <th></th>
+      </tr>
+    </thead>`;
+  formData.map((currFormData) => {
+    const lastOpenedAt = currFormData.lastOpenedAt;
+    allForms.innerHTML += `
+      <tbody>
+        <tr>
+          <td class="d-flex align-items-center py-3">
+            <i class="fa-solid fa-list mt-1"></i>
+            <h6 class="ms-5 me-3 mb-1 mt-1">${currFormData.title}</h6>
+            <i class="fa-solid fa-user-group text-secondary"></i>
+          </td>
+          <td class="align-middle">${currFormData.owners
+            .map((currOwner) => currOwner.mailId.split("@")[0])
+            .join(", ")}</td>
+          <td class="align-middle">${lastOpenedAt.getDate()} ${
+      months[lastOpenedAt.getMonth()]
+    } ${lastOpenedAt.getFullYear()}</td>
+          <td class="align-middle">
+            <button
+              class="btn btn-outline-secondary rounded-circle border-0 px-3"
+            >
+              <i class="fa-solid fa-ellipsis-vertical"></i>
+            </button>
+          </td>
+        </tr>
+      </tbody>
+  `;
+  });
+  allFormsContainer.appendChild(allForms);
+};
+
+viewToggle.addEventListener("click", () => {
+  currView === "grid" ? "list" : "grid";
+
+  switch (currView) {
+    case "grid":
+      currView = "list";
+      break;
+    default:
+      currView = "grid";
+      break;
+  }
+
+  currView === "grid"
+    ? displayAllFormsInGrid(formData)
+    : displayAllFormsInList(formData);
+});
+
+currView === "grid"
+  ? displayAllFormsInGrid(formData)
+  : displayAllFormsInList(formData);
